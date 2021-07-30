798876e5aff8b48a22c84c4d2d9bad23cacf3221
ozone671game <sapog.strausa@mail.ru>
2021-07-30 03:26:54 +0300

    learning...

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OOOOOOOOOLALA',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -31,38 +29,113 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+final List<Widget>_tabs = [
+  AccountTab(),
+  AddPhotoTab(),
+  MenuTab(),
+  SettingTab()
+];
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: Center(
-        child: CupertinoButton(
-          color: Colors.deepPurpleAccent,
-          onPressed: () {
-            showCupertinoModalPopup<void>(
-              context: context,
-              builder: (BuildContext context) => CupertinoActionSheet(
-                title: const Text('Choose 1 or 2'),
-                message: const Text('Types of ....'),
-                actions: <CupertinoActionSheetAction>[
-                  CupertinoActionSheetAction(
-                    child: const Text('Action 1'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  CupertinoActionSheetAction(
-                    child: const Text('Action 2'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  )
-                ],
-              ),
-            );
-          },
-          child: const Text('CupertinoActionSheet'),
-        ),
-      ),
+    return CupertinoTabScaffold(
+        backgroundColor: Colors.indigoAccent,
+        tabBar: CupertinoTabBar(
+          activeColor: Colors.deepPurple,
+          backgroundColor: Colors.grey,
+          inactiveColor: Colors.deepPurpleAccent,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_outlined), label: 'Account'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add_a_photo_outlined), label: 'Add Photo'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.widgets_outlined), label: 'Menu'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.build), label: 'Setting')
+      ],
+    ),
+    tabBuilder: (BuildContext context, index){
+          return _tabs[index];
+    },
+    );
+  }
+}
+
+class Picker extends MenuTab {
+
+}
+
+class MenuTab extends StatelessWidget {
+
+  late int selectedValue = 7;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.black,
+      // padding: EdgeInsets.all(2.0),
+      // margin: EdgeInsets.all(2.0),
+      alignment: Alignment.topCenter,
+      child: CupertinoPicker(
+        itemExtent: 32.0,
+        onSelectedItemChanged: (value){setState(){selectedValue = value;}},
+        scrollController: null,
+        children: const[
+          Text('1'),
+          Text('2'),
+          Text('3'),
+          Text('4'),
+          Text('5'),
+          Text('6'),
+          Text('7'),
+        ]),
+    );
+  }
+}
+
+
+
+class AccountTab extends StatelessWidget {
+  String a = 'GO';
+
+  void textChange() async {
+     a = 'GONE';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:      Container(
+      alignment: Alignment.topCenter,
+      width: 400,
+      height: 150,
+      color: Colors.white10,
+      child: CupertinoButton(
+          alignment: Alignment.topCenter,
+          child: Text(a),
+          onPressed: textChange),
+      // Text('Account Tab'),
+    ),
+    );
+  }
+}
+
+class AddPhotoTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('AddPhotoTab'),
+    );
+  }
+}
+
+
+class SettingTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Settings Tab'),
     );
   }
 }
